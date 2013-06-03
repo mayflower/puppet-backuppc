c03efef93c33448eb99d2ac97e7f8d742bacb355
Tristan Helmich <tristan.helmich@mayflower.de>
2013-06-03 10:56:51 +0200

    quick hack to ensure apache2 service is restarted after config has been placed

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,7 +1,14 @@
 class backuppc::apache inherits backuppc::params {
+  package { 'apache2':
+    ensure => installed,
+  }
+  service { 'apache2':
+    ensure => running,
+  }
   file { $config_apache:
     ensure  => symlink,
     target  => '/etc/backuppc/apache.conf',
-    require => Package[$package]
+    require => Package[$package],
+    notify  => Service['apache2'],
   }
 }
